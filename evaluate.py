be93e439b60de209a985d24fc543674fe45fe276
draichi <lucasdraichi@gmail.com>
2018-08-09 19:45:04 -0300

    fancy terminal

--- evaluate.py
+++ evaluate.py
@@ -10,6 +10,7 @@ from agent.agent import Agent
 from functions import *
 from my_vars import days, currency
 
+width = os.get_terminal_size().columns
 asset_name, model_name = sys.argv[1], sys.argv[2]
 model = load_model("models/" + model_name)
 window_size = model.layers[0].input.shape.as_list()[1]
@@ -23,47 +24,33 @@ state = getState(data, 0, window_size + 1)
 total_profit = 0
 agent.inventory = []
 
-width = os.get_terminal_size().columns
 print(chr(27) + "[2J")
 print(colored('{}/{}'.format(asset_name.upper(), currency.upper()).center(width), 'blue'))
 print(colored('Model {}\n\n'.format(model_name).center(width), 'blue'))
 print(colored('From 1 ~~> {} {:.7f}'.format(currency.upper(),data[0]).center(width), 'magenta'))	
 print(colored('To {} ~~> {} {:.7f}\n\n'.format(l,currency.upper(),data[-1]).center(width), 'magenta'))	
-# print(colored('> To  {} ~~> {} {:.7f}\n\n'.format(l, currency.upper(), data[-1]), 'magenta'))	
+
 for t in range(l):
 	action = agent.act(state)
-
-	# sit
 	next_state = getState(data, t + 1, window_size + 1)
 	reward = 0
-	print("> {} BTC {:.7f}".format(t, data[t]), end='\r')
-
+	print("> {} BTC {:.7f}".format(t, data[t]), end='\r') #hold
 	if action == 1: # buy
 		agent.inventory.append(data[t])
 		print(colored("> {} BTC {:.7f} |".format(t, data[t]), 'cyan'), formatPrice(total_profit), end='\r')
-		# print(colored("> BUY {0:.7f}  - Total profit: {}".format(data[t], total_profit), 'cyan'), end='\r')
-		
-
 	elif action == 2 and len(agent.inventory) > 0: # sell
 		bought_price = agent.inventory.pop(0)
 		reward = max(data[t] - bought_price, 0)
 		total_profit += data[t] - bought_price
 		print(colored("> {} BTC {:.7f} |".format(t, data[t]), 'yellow'), formatPrice(total_profit), end='\r')
-		# print(colored("> SELL {0:.7f}  - Total profit: {}".format(data[t], total_profit), 'red'), end='\r')
-	# elif action == 0:
-	# 	print(colored("> HOLD {0:.7f}  - Total profit: {}".format(data[t], total_profit), 'yellow'), end='\r')
-
 	done = True if t == l - 1 else False
 	agent.memory.append((state, action, reward, next_state, done))
 	state = next_state
-
 	if done:
 		print('\n\n')
 		print(colored("-----------------------------".center(width), 'cyan'))
 		print(formatPrice(total_profit).center(width))
 		print(colored("-----------------------------".center(width),'cyan'))
-
 	if len(agent.memory) > batch_size:
 		agent.expReplay(batch_size)
-
 print(colored('D O N E'.center(width),'white','on_green',attrs=['bold']))
--- train.py
+++ train.py
@@ -1,58 +1,73 @@
-from agent.agent import Agent
-from functions import *
 import sys
 from termcolor import colored
 
-try:
-	if len(sys.argv) != 4:
-		print(colored("Usage: python train.py [stock] [window] [episodes]", 'red'))
-		exit()
-	from my_vars import stock_name, window_size, episode_count
-
-	agent = Agent(window_size)
-	data = getStockDataVec(stock_name)
-	l = len(data) - 1
-	batch_size = 32
-
-	for e in range(episode_count + 1):
-		print(colored("      {}/BTC".format(stock_name), 'yellow', attrs=['bold']))
-		print(colored("> Episode {}/{}\n".format(str(e), str(episode_count)), 'yellow',  attrs=['bold']))
-		state = getState(data, 0, window_size + 1)
-
-		total_profit = 0
-		agent.inventory = []
-
-		for t in range(l):
-			action = agent.act(state)
-
-			# sit
-			next_state = getState(data, t + 1, window_size + 1)
-			reward = 0
-
-			if action == 1: # buy
-				agent.inventory.append(data[t])
-				print(colored("> BTC {0:.7f}".format(data[t]), 'cyan'))
-
-			elif action == 2 and len(agent.inventory) > 0: # sell
-				bought_price = agent.inventory.pop(0)
-				reward = max(data[t] - bought_price, 0)
-				total_profit += data[t] - bought_price
-				print(colored("> BTC {0:.7f} |".format(data[t]), 'yellow'), formatPrice(data[t] - bought_price))
-			done = True if t == l - 1 else False
-			agent.memory.append((state, action, reward, next_state, done))
-			state = next_state
-
-			if done:
-				print(colored("\n\n\n\n\n------TOTAL PROFIT------\n", 'cyan'))
-				print('   '+ formatPrice(total_profit))
-				print(colored("\n------------------------\n",'cyan'))
-
-			if len(agent.memory) > batch_size:
-				agent.expReplay(batch_size)
-
-		if e % 10 == 0:
-			agent.model.save("models/{}_e{}_w{}_c{}".format(stock_name, str(e), window_size, episode_count))
-
-finally:
-	print(colored('               D O N E              ', 'white', 'on_green', attrs=['bold']))
+if len(sys.argv) != 4:
+	print(colored("Usage: python train.py [asset] [window] [episodes]", 'red'))
 	exit()
+import os
+from agent.agent import Agent
+from functions import getState, getStockDataVec, sigmoid
+from my_vars import days, currency
+
+
+
+
+asset_name = sys.argv[1]
+window_size, episode_count = int(sys.argv[2]), int(sys.argv[3])
+
+width = os.get_terminal_size().columns
+asset = '{}_d{}_{}'.format(asset_name, days, currency)	
+
+agent = Agent(window_size)
+data = getStockDataVec(asset)
+l = len(data) - 1
+batch_size = 32
+
+# prints formatted price
+def formatPrice(n):
+	if n < 0:
+		return colored('Total profit: -BTC {0:.6f}'.format(abs(n)), 'red', attrs=['bold'])
+	else:
+		return colored('Total profit: BTC {0:.7f}'.format(abs(n)), 'green', attrs=['bold'])
+	# return ("-BTC " if n < 0 else "BTC ") + "{0:.7f}".format(abs(n))
+
+
+
+print(chr(27) + "[2J")
+print(colored('{}/{}'.format(asset_name.upper(), currency.upper()).center(width), 'blue'))
+print(colored('Trainning \n\n'.center(width), 'blue'))
+print(colored('From 1 ~~> {} {:.7f}'.format(currency.upper(),data[0]).center(width), 'magenta'))	
+print(colored('To {} ~~> {} {:.7f}\n\n'.format(l,currency.upper(),data[-1]).center(width), 'magenta'))	
+
+for e in range(episode_count+1):
+    print(colored("      {}/BTC".format(asset_name).center(width), 'yellow', attrs=['bold']))
+    print(colored("> Episode {}/{}\n".format(str(e), str(episode_count)).center(width), 'yellow',  attrs=['bold']))
+    state = getState(data, 0, window_size + 1)
+    total_profit = 0
+    agent.inventory = []
+    for t in range(l):
+        action = agent.act(state)
+        next_state = getState(data, t + 1, window_size + 1)
+        reward = 0
+        print("> {} BTC {:.7f}".format(t, data[t]), end='\r') #hold
+        if action == 1: # buy
+            agent.inventory.append(data[t])
+            print(colored("> {} BTC {:.7f} |".format(t, data[t]), 'cyan'), end='\r')
+        elif action == 2 and len(agent.inventory) > 0: # sell
+            bought_price = agent.inventory.pop(0)
+            reward = max(data[t] - bought_price, 0)
+            total_profit += data[t] - bought_price
+            print(colored("> {} BTC {:.7f} |".format(t, data[t]), 'yellow'), formatPrice(total_profit), end='\r')
+        done = True if t == l - 1 else False
+        agent.memory.append((state, action, reward, next_state, done))
+        state = next_state
+        if done:
+            print('\n\n')
+            print(colored("-----------------------------".center(width), 'cyan'))
+            print(formatPrice(total_profit).center(width))
+            print(colored("-----------------------------".center(width),'cyan'))
+        if len(agent.memory) > batch_size:
+            agent.expReplay(batch_size)
+    if e % 10 == 0:
+        agent.model.save("models/{}_d{}_e{}_w{}_c{}".format(days, asset_name, str(e), window_size, episode_count))
+print(colored('D O N E'.center(width),'white','on_green',attrs=['bold']))
